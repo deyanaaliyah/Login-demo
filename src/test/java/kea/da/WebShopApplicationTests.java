ae7be003ca4637dd86293bec3c9d87571c9edf9d
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-09-10 09:13:35 +0200

    Initial commit

---
+++ src/test/java/kea/da/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
